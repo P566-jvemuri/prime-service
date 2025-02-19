fe011bf80b59e9025fa97353aded09534b11f3aa
jahnavi-vemuri <jvemuri@iu.edu>
2025-02-18 21:01:33 -0500

    practicum1

--- prime-service/src/main/java/edu/indiana/p566/prime_service/controller/PrimesController.java
+++ prime-service/src/main/java/edu/indiana/p566/prime_service/controller/PrimesController.java
@@ -1,6 +1,21 @@
 package edu.indiana.p566.prime_service.controller;
 
+import edu.indiana.p566.prime_service.service.IPrimeService;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
 public class PrimesController {
 
+    private final IPrimeService primesService;
+
+    public PrimesController(IPrimeService primesService) {
+        this.primesService = primesService;
+    }
+
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable long n) { // Match the method signature
+        return primesService.isPrime(n);
+    }
 }
---
+++ prime-service/src/main/java/edu/indiana/p566/prime_service/service/IPrimeService.java
@@ -0,0 +1,5 @@
+package edu.indiana.p566.prime_service.service;
+
+public interface IPrimeService {
+    boolean isPrime(long n);
+}
