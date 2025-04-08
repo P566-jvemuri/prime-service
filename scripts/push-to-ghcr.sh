aebac65078b1944f7ccaf254c8e4a69a0b37b5fd
jahnavi-vemuri <jvemuri@iu.edu>
2025-04-08 16:32:22 -0400

    practi9

--- scripts/push-to-ghcr.sh
+++ scripts/push-to-ghcr.sh
@@ -1,5 +1,5 @@
 set -u
 : "$VERSION"
 
-echo ${ACCESS_TOCKEN} | docker login ghcr.io --username jahnavi-vemuri --password-stdin
+echo ${secrets.ACCESS_TOCKEN} | docker login ghcr.io --username jahnavi-vemuri --password-stdin
 docker push ghcr.io/jahnavi-vemuri/prime-service:$VERSION
